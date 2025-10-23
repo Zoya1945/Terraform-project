30054a98086ed79382714b9333465a9722fe823e
Zoya1945 <zoyazh1945@gmail.com>
2025-10-24 00:43:21 +0530

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
-resource "" "name" {
+resource "aws_instance" "zterraform" {
+    instance_type = "t3.micro"
+    ami = "ami-0f9708d1cd2cfee41"
+    tags = {
+         Name = "Terraform"
+    }
+   
+  
+}
+resource "aws_instance" "oterraform" {
+    instance_type = "t3.micro"
+    ami = "ami-0f9708d1cd2cfee41"
+    tags = {
+         Name = "Oerraform"
+    }
+   
   
 }
